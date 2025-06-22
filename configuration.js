781daa73a20daa5c8b28f11630669cc71f9c8de9
Laylead <161890363+Laylead@users.noreply.github.com>
2025-06-22 10:30:12 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -7,8 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "593310501630",
   appId: "1:593310501630:web:c241a05e0d7c4a810173af"
 };
-
-const adminCredentials = {
-  email: "admin@leadconnect.com",
-  password: "adminPassword123"
-};
