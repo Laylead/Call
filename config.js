3262d1e8e188851c951e14332f7be5ebaf36fc8f
Laylead <161890363+Laylead@users.noreply.github.com>
2025-10-30 20:46:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyBlh428ba5aSFt6x2pPkVjeEk5D6AbIU7Y",
-  authDomain: "kheemz-fb9b6.firebaseapp.com",
-  databaseURL: "https://kheemz-fb9b6-default-rtdb.firebaseio.com",
-  projectId: "kheemz-fb9b6",
-  storageBucket: "kheemz-fb9b6.appspot.com",
-  messagingSenderId: "593310501630",
-  appId: "1:593310501630:web:c241a05e0d7c4a810173af"
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBlh428ba5aSFt6x2pPkVjeEk5D6AbIU7Y",
+    authDomain: "kheemz-fb9b6.firebaseapp.com",
+    databaseURL: "https://kheemz-fb9b6-default-rtdb.firebaseio.com",
+    projectId: "kheemz-fb9b6",
+    storageBucket: "kheemz-fb9b6.appspot.com",
+    messagingSenderId: "593310501630",
+    appId: "1:593310501630:web:c241a05e0d7c4a810173af"
 };
-
-
