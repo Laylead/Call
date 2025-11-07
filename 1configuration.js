29fefda54451c0dfb90e7f1ebd33173051aab4a5
Laylead <161890363+Laylead@users.noreply.github.com>
2025-11-07 15:35:26 +0100

    Create 1configuration.js

---
+++ 1configuration.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyClgURahj1cRQRK0bGEDZwwosAWHpXVmwM",
+  authDomain: "netgrant-66de8.firebaseapp.com",
+  projectId: "netgrant-66de8",
+  storageBucket: "netgrant-66de8.firebasestorage.app",
+  messagingSenderId: "190988981153",
+  appId: "1:190988981153:web:9b5f92a3dee6a38ab8f1e0",
+  measurementId: "G-6B2NQ74FG0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
