7ee7d54c3837d29c04ed08c64415e2b970375394
Laylead <161890363+Laylead@users.noreply.github.com>
2025-11-20 23:14:40 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -8,3 +8,6 @@ const firebaseConfig = {
     messagingSenderId: "593310501630",
     appId: "1:593310501630:web:c241a05e0d7c4a810173af"
 };
+
+window.firebaseConfig = firebaseConfig;
+window.CONFIG = CONFIG;
